e74980c2fe63aaa8f24dfac7c58f0340f692bd50
jhn-cde <toe.johan@unsaac.edu.pe>
2022-11-12 12:17:38 -0500

    edit OrderScreen

--- cliente/src/AppRouter.js
+++ cliente/src/AppRouter.js
@@ -2,15 +2,21 @@ import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import ProductsScreen from './screens/ProductsScreen'
 import OrdersScreen from './screens/OrdersScreen'
+import OrderScreen from './screens/OrderScreen'
 import Navbar from './components/Navbar'
+
 const AppRouter = () => {
   return(
     <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<OrdersScreen />}/>
-        <Route path="/products" element={<ProductsScreen />}/>
-      </Routes>
+      <div className='container mt-3'>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<OrdersScreen />}/>
+          <Route path="/orders" element={<OrdersScreen />}/>
+          <Route path="/products" element={<ProductsScreen />}/>
+          <Route path="/orders/:orderid" element={<OrderScreen />}/>
+        </Routes>
+      </div>
     </BrowserRouter>
   )
 }
