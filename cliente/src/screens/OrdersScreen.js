c8ebbfb17e8dc9947f7f780b2c2ae195d9476a1b
jhn-cde <toe.johan@unsaac.edu.pe>
2022-11-12 19:59:35 -0500

    pagination

--- cliente/src/screens/OrdersScreen.js
+++ cliente/src/screens/OrdersScreen.js
@@ -1,87 +1,82 @@
 import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from 'react'
+import { selectOrders } from '../actions/ordersSlice';
+import { useAppSelector } from '../hooks';
+import { calculateRange, sliceData } from '../utils/divdeList';
 
-const ordersList = [
-  {
-    id: 1,
-    Consumer: 'Joe Smith',
-    Status: 'Pending',
-    Date: '05/02/2021',
-    Total: 127.23 
-  },
-  {
-    id: 2,
-    Consumer: 'Joe Baker',
-    Status: 'Completed',
-    Date: '05/02/2021',
-    Total: 50.00
-  },
-  {
-    id: 3,
-    Consumer: 'Joe Adams',
-    Status: 'Rejected',
-    Date: '05/01/2021',
-    Total: 200.50 
-  },
-  {
-    id: 4,
-    Consumer: 'Joseph Evans',
-    Status: 'Completed',
-    Date: '04/30/2021',
-    Total: 100 
-  },
-]
-
+const rowsPerPage = 4
 const OrdersScreen = () => {
+  const ordersList = useAppSelector(selectOrders)
+  const [page, setPage] = useState(1)
+  const [slice, setSlice] = useState([])
+  const [range, setRange] = useState([])
+
+  useEffect(() => {
+    const range = calculateRange(ordersList, rowsPerPage)
+    setRange([...range])
+    const slice = sliceData(ordersList, rowsPerPage, page);
+    setSlice([...slice]);
+  }, [ordersList, page]);
+
   return(
     <div className='ProductsScreen'>
       <h1 className='mt-3'>Orders</h1>
       <div className='d-flex justify-content-end mb-4'>
-        <Link to={`/products`} className='btn btn-primary'>
+        <Link to={`/orders/create`} className='btn btn-primary'>
           Create Order
         </Link>
       </div>
-      <table class="table table-striped">
-        <thead>
-          <tr>
-            <th scope='col'>N°</th>
-            <th scope='col'>Consumer</th>
-            <th scope='col'>Status</th>
-            <th scope='col'>Date</th>
-            <th scope='col'>Total</th>
-            <th scope='col'>Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          {
-            ordersList.map(order => {
-              return (
-                <tr key={order.id}>
-                  <td>{order.id}</td>
-                  <td>{order.Consumer}</td>
-                  <td>{order.Status}</td>
-                  <td>{order.Date}</td>
-                  <td>{order.Total}</td>
-                  <td>
-                    <Link
-                      to={`/orders/${order.id}`}
-                    >
-                      <li className='btn btn-link'>Edit</li>
-                    </Link>
-                  </td>
-                </tr>
-              )
-            })
-          }
-        </tbody>
-      </table>
+      <div style={{height: 300}}>
+        <table className="table table-striped">
+          <thead>
+            <tr>
+              <th scope='col'>N°</th>
+              <th scope='col'>Consumer</th>
+              <th scope='col'>Status</th>
+              <th scope='col'>Date</th>
+              <th scope='col'>Total</th>
+              <th scope='col'>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              slice.map(order => {
+                return (
+                  <tr key={order.Number}>
+                    <td>{order.Number}</td>
+                    <td>{order.Consumer}</td>
+                    <td>{order.Status}</td>
+                    <td>{order.Date}</td>
+                    <td>{order.Subtotal}</td>
+                    <td>
+                      <Link
+                        to={`/orders/${order.Number}`}
+                      >
+                        <li className='btn btn-link'>Edit</li>
+                      </Link>
+                    </td>
+                  </tr>
+                )
+              })
+            }
+          </tbody>
+        </table>
+      </div>
       <div className='d-flex justify-content-end mb-3'>
         <nav aria-label="Page navigation example">
           <ul className="pagination">
-            <li className="page-item"><button className="page-link" >Previous</button></li>
-            <li className="page-item"><button className="page-link" >1</button></li>
-            <li className="page-item"><button className="page-link" >2</button></li>
-            <li className="page-item"><button className="page-link" >3</button></li>
-            <li className="page-item"><button className="page-link" >Next</button></li>
+            {
+              range.map(page =>
+                <li className="page-item" key={page}>
+                  <button
+                    className="page-link"
+                    onClick={() => setPage(page)}
+                  >
+                    {page}
+                  </button>
+                </li> 
+              )
+            }
           </ul>
         </nav>
       </div>
