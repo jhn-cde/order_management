879f25505b939f461ef0fb9970ddb04d8afdb9a7
jhn-cde <toe.johan@unsaac.edu.pe>
2022-11-14 17:35:52 -0500

    use ProductsTable component

--- cliente/src/components/ui/AddItems.js
+++ cliente/src/components/ui/AddItems.js
@@ -47,57 +47,58 @@ const AddItems = ({children, onSaveItems, products=[]}) => {
   }
 
   return(
-        <div>
-          <div className="row d-flex justify-content-between">
-            {children}
-            <div className="col-auto">
-              <h3>Added products</h3>
-              <ul className="list-group mb-3">
-                {addedProducts.map((product) => {
-                  return(
-                    <li key={product.id} className='list-group-item d-flex justify-content-between'>
-                      {`${product.Name} - $${product.Unitprice}`}
-                      <div className="d-flex justify-content-between">
-                        <div className="input-group ms-3 input-group-sm" style={{width:'110px', height:'25px'}}>                        
-                          <input
-                            type="number" 
-                            style={{height:'25px'}}
-                            className="form-control ms-2 me-3"
-                            min={1}
-                            name='Quantity'
-                            value={product.Quantity}
-                            onChange={({target}) => changeQuantity({value: target.value, productid: product.id})}
-                          />
-                        </div>
-                        <div>
-                          <button
-                            className="btn btn-link p-0" 
-                            style={{width:20, height:25}}
-                            onClick={() => removeProduct(product.id)}>x</button>
-                        </div>
-                      </div>
-                    </li>
-                )})}
-              </ul>
-              <p>SubTotal: {subTotal}</p>
-            </div>
-          </div>
-
-          <div className="mb-3 mt-3">
-            <h3>Products</h3>
-            <ProductsTable
-              productsList={productsList.filter(p => !addedProducts.find(added => added.id === p.id))}
-              actions={[{name:'Add', action: addProduct}]}
-            />
-          </div>
-
-          <button 
-            className="btn btn-primary btn-lg"
-            onClick={onSave}
-          >
-            Save
-          </button>
+    <div>
+      <div className="row d-flex justify-content-between">
+        {children}
+        <div className="col-auto">
+          <h3>Added products</h3>
+          <ul className="list-group mb-3">
+            {addedProducts.map((product) => {
+              return(
+                <li key={product.id} className='list-group-item d-flex justify-content-between'>
+                  {`${product.Name} - $${product.Unitprice}`}
+                  <div className="d-flex justify-content-between">
+                    <div className="input-group ms-3 input-group-sm" style={{width:'110px', height:'25px'}}>                        
+                      <input
+                        type="number" 
+                        style={{height:'25px'}}
+                        className="form-control ms-2 me-3"
+                        min={1}
+                        name='Quantity'
+                        value={product.Quantity}
+                        onChange={({target}) => changeQuantity({value: target.value, productid: product.id})}
+                      />
+                    </div>
+                    <div>
+                      <button
+                        className="btn btn-link p-0" 
+                        style={{width:20, height:25}}
+                        onClick={() => removeProduct(product.id)}>x</button>
+                    </div>
+                  </div>
+                </li>
+            )})}
+          </ul>
+          <p>SubTotal: {subTotal}</p>
         </div>
+      </div>
+
+      <div className="mb-3 mt-3">
+        <h3>Products</h3>
+        <ProductsTable
+          productsList={productsList.filter(p => !addedProducts.find(added => added.id === p.id))}
+          rowsPerPage={3}
+          actions={[{name:'Add', action: addProduct}]}
+        />
+      </div>
+
+      <button 
+        className="btn btn-primary btn-lg"
+        onClick={onSave}
+      >
+        Save
+      </button>
+    </div>
   )
 }
 
