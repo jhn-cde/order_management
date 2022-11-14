affd4e3a138964ef934f27f5232e53dbf2cc8cdc
jhn-cde <toe.johan@unsaac.edu.pe>
2022-11-14 15:32:22 -0500

    add, remove, edit items

---
+++ cliente/src/components/ui/AddItems.js
@@ -0,0 +1,103 @@
+
+import "react-datepicker/dist/react-datepicker.css";
+import ProductsTable from "../ui/ProductsTable";
+import { useSelector } from "react-redux";
+import { selectProducts } from "../../actions/productsSlice";
+import { useState } from "react";
+
+
+const AddItems = ({children, onSaveItems, products=[]}) => {
+  const productsList = useSelector(selectProducts)
+  const [addedProducts, setAddedProducts] = useState(products)
+  const [subTotal, setSubTotal] = useState(0)
+
+  const addProduct = (productid) => {
+    const product = productsList.filter(p => p.id === productid)[0]
+    const toadd = {
+      id: productid,
+      Name: product.Name,
+      Quantity: 1,
+      Unitprice: product.Price
+    }
+    
+    setSubTotal(subTotal + product.Price)
+    setAddedProducts([...addedProducts, toadd])
+  }
+  
+  const changeQuantity = ({value, productid}) => {
+    const addedList = addedProducts.map(p => {
+      p.Quantity = productid===p.id? value: p.Quantity
+      return p
+    })
+    setAddedProducts(addedList)
+    const tmpSubTotal = addedList.reduce((ac, product) => ac + product.Unitprice*product.Quantity, 0)
+    setSubTotal(tmpSubTotal)
+  }
+
+  const removeProduct = (productid) => {
+    const addedList = addedProducts.filter(p => p.id !== productid)
+    setAddedProducts(addedList)
+    const subTotal = addedList.reduce((ac, product) => ac + product.Unitprice*product.Quantity, 0)
+    setSubTotal(subTotal)
+  }
+
+  const onSave = () => {
+    onSaveItems({products: addedProducts, Subtotal: subTotal})
+  }
+
+  return(
+        <div>
+          <div className="row d-flex justify-content-between">
+            {children}
+            <div className="col-auto">
+              <h3>Added products</h3>
+              <ul className="list-group mb-3">
+                {addedProducts.map((product) => {
+                  return(
+                    <li key={product.id} className='list-group-item d-flex justify-content-between'>
+                      {`${product.Name} - $${product.Unitprice}`}
+                      <div className="d-flex justify-content-between">
+                        <div className="input-group ms-3 input-group-sm" style={{width:'110px', height:'25px'}}>                        
+                          <input
+                            type="number" 
+                            style={{height:'25px'}}
+                            className="form-control ms-2 me-3"
+                            min={1}
+                            name='Quantity'
+                            value={product.Quantity}
+                            onChange={({target}) => changeQuantity({value: target.value, productid: product.id})}
+                          />
+                        </div>
+                        <div>
+                          <button
+                            className="btn btn-link p-0" 
+                            style={{width:20, height:25}}
+                            onClick={() => removeProduct(product.id)}>x</button>
+                        </div>
+                      </div>
+                    </li>
+                )})}
+              </ul>
+              <p>SubTotal: {subTotal}</p>
+            </div>
+          </div>
+
+          <div className="mb-3 mt-3">
+            <h3>Products</h3>
+            <ProductsTable
+              productsList={productsList.filter(p => !addedProducts.find(added => added.id === p.id))}
+              actions={[{name:'Add', action: addProduct}]}
+            />
+          </div>
+
+          <button 
+            className="btn btn-primary btn-lg"
+            onClick={onSave}
+          >
+            Save
+          </button>
+        </div>
+  )
+}
+
+export default AddItems
