f2a8f05bfc81a36c988eeac01fe8c2e4d5af3b84
jhn-cde <toe.johan@unsaac.edu.pe>
2022-11-13 22:20:53 -0500

    remove pagination

--- cliente/src/components/ProductsTable.js
+++ cliente/src/components/ProductsTable.js
@@ -1,23 +1,7 @@
-import { useEffect, useState } from "react"
-import { calculateRange, sliceData } from "../utils/divdeList"
-
-const rowsPerPage = 4
-
 const ProductsTable = ({productsList, actions}) => {
-  const [page, setPage] = useState(1)
-  const [slice, setSlice] = useState([])
-  const [range, setRange] = useState([])
-
-  useEffect(() => {
-    const range = calculateRange(productsList, rowsPerPage)
-    setRange([...range])
-    const slice = sliceData(productsList, rowsPerPage, page);
-    setSlice([...slice]);
-  }, [productsList, page]);
-
   return (
-    <div style={{height: 300}}>
-      <table className="table table-striped">
+    <div>
+        <table className="table table-striped">
           <thead>
             <tr>
               <th scope='col'>N°</th>
@@ -30,7 +14,7 @@ const ProductsTable = ({productsList, actions}) => {
           </thead>
           <tbody>
             {
-              slice.map(product => {
+              productsList.map(product => {
                 return (
                   <tr key={product.id}>
                     <td>{product.id}</td>
@@ -52,26 +36,7 @@ const ProductsTable = ({productsList, actions}) => {
             }
           </tbody>
         </table>
-        <div className='d-flex justify-content-end mb-3'>
-        <nav aria-label="Page navigation example">
-          <ul className="pagination">
-            {
-              range.map(page =>
-                <li className="page-item" key={page}>
-                  <button
-                    className="page-link"
-                    onClick={() => setPage(page)}
-                  >
-                    {page}
-                  </button>
-                </li> 
-              )
-            }
-          </ul>
-        </nav>
-      </div>
     </div>
-      
   )
 }
 
