7723d59142d9f419702b6866b36a1b642a402451
jhn-cde <toe.johan@unsaac.edu.pe>
2022-11-14 16:32:33 -0500

    edit order

--- routes/norder.js
+++ routes/norder.js
@@ -25,9 +25,8 @@ const schemeOrder = new scheme({
 const ModelOrder = mongoose.model('norders', schemeOrder)
 module.exports = router
 
-
-router.post('/addorder', (req, res) => {
-  let Total = req.body.Subtotal
+const getTaxes = (Subtotal) => {
+  let Total = Subtotal
   let Taxes = {
     CityTax: 0, CountyTax: 0, StateTax: 0, FederalTax: 0
   }
@@ -39,21 +38,26 @@ router.post('/addorder', (req, res) => {
   Taxes.StateTax = Math.round(Total*0.08 * 100) / 100
   Total+=Taxes.StateTax
   Taxes.FederalTax = Math.round(Total*0.02 * 100) / 100
-  Total+=Taxes.FederalTax
+  Total=Math.round((Total+Taxes.FederalTax)*100)/100
+  
+  let TotalTaxes = (Taxes.CityTax 
+    + Taxes.CountyTax 
+    + Taxes.StateTax
+    + Taxes.FederalTax)
+  TotalTaxes = Math.round(TotalTaxes*100)/100
+  return {Taxes, TotalTaxes, Total}
+}
 
-  Total = Math.round(Total*100)/100
+
+router.post('/addorder', (req, res) => {
+  const Taxes = getTaxes(req.body.Subtotal)
   const newOrder = ModelOrder({
+    ...Taxes,
     Number: req.body.Number,
     Status: req.body.Status,
     Date: req.body.Date,
     Consumer: req.body.Consumer,
     Subtotal: req.body.Subtotal,
-    Taxes: Taxes,
-    TotalTaxes: Math.round((Taxes.CityTax 
-      + Taxes.CountyTax 
-      + Taxes.StateTax
-      + Taxes.FederalTax)*100)/100,
-    Total: Total,
     products: req.body.products
   })
   newOrder.save((err) => {
@@ -68,7 +72,7 @@ router.post('/addorder', (req, res) => {
 
 router.get('/getorders', (req, res) => {
   console.log('getorders')
-  ModelOrder.find({}, (docs, err) => {
+  ModelOrder.find({}, (err, docs) => {
     if(!err){
       res.send(docs)
     }else{
@@ -77,13 +81,33 @@ router.get('/getorders', (req, res) => {
   })
 })
 
-router.post('/getorders', (req, res) => {
-  ModelOrder.find({Number:req.body.Number}, (docs, err) => {
+router.post('/editOrderStatus', (req, res) => {
+  ModelOrder.findOneAndUpdate({Number: req.body.Number}, {
+    Status: req.body.Status
+  }, (err) => {
     if(!err){
-      console.log('success', docs)
-      res.send(docs)
+      res.send(`Status changed successfully, ${req.body.Status}`)
     }else{
-      console.log('error! getorders ', err)
+      console.log('error! editOrderStatus ', err)
+      res.send(err)
+    }
+  })
+})
+
+router.post('/editOrderProducts', (req, res) => {
+  const Subtotal = req.body.products.reduce((ac, product)=> 
+    ac + product.Unitprice*product.Quantity, 0)
+  const Taxes = getTaxes(Subtotal)
+  const toChange = {
+    ...Taxes,
+    products: req.body.products,
+    Subtotal
+  }
+  ModelOrder.findOneAndUpdate({Number: req.body.Number}, toChange, (err) => {
+    if(!err){
+      res.send('Products changed successfully')
+    }else{
+      console.log('error! editOrderProducts ', err)
       res.send(err)
     }
   })
