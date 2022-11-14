99081259e97d134d1a931e92e08bb54df0397128
jhn-cde <toe.johan@unsaac.edu.pe>
2022-11-14 17:34:39 -0500

    remove pagination

--- cliente/src/components/products/ProductsScreen.js
+++ cliente/src/components/products/ProductsScreen.js
@@ -1,26 +1,13 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import { Link, useNavigate } from 'react-router-dom';
 import { deleteProduct, selectProducts } from '../../actions/productsSlice';
 import ProductsTable from '../ui/ProductsTable';
 import { useDispatch, useSelector } from "react-redux";
-import { calculateRange, sliceData } from '../../utils/divideList';
 
-const rowsPerPage = 4
 const ProductsScreen = () => {
   const dispatch = useDispatch()
   const navigate = useNavigate()
   const productsList = useSelector(selectProducts)
-
-  const [page, setPage] = useState(1)
-  const [slice, setSlice] = useState([])
-  const [range, setRange] = useState([])
-
-  useEffect(() => {
-    const range = calculateRange(productsList, rowsPerPage)
-    setRange([...range])
-    const slice = sliceData(productsList, rowsPerPage, page);
-    setSlice([...slice]);
-  }, [productsList, page]);
 
   const editProd = (id) => {
     navigate(`/products/${id}`)
@@ -38,30 +25,13 @@ const ProductsScreen = () => {
       </div>
       <div style={{height: 300}}>
         <ProductsTable 
-          productsList={slice}
+          productsList={productsList}
+          rowsPerPage={4}
           actions={[
             {name: 'Edit', action: editProd},
             {name: 'Delete', action: deleteProd}
           ]}
         />
-      </div>
-      <div className='d-flex justify-content-end mb-3'>
-        <nav aria-label="Page navigation example">
-          <ul className="pagination">
-            {
-              range.map(page =>
-                <li className="page-item" key={page}>
-                  <button
-                    className="page-link"
-                    onClick={() => setPage(page)}
-                  >
-                    {page}
-                  </button>
-                </li> 
-              )
-            }
-          </ul>
-        </nav>
       </div>
     </div>
   )
