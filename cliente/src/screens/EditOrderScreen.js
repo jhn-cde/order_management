34e2cb935a39bb45b6a73605218b561c469a76bd
jhn-cde <toe.johan@unsaac.edu.pe>
2022-11-13 22:20:03 -0500

    using bootstap list-group

--- cliente/src/screens/EditOrderScreen.js
+++ cliente/src/screens/EditOrderScreen.js
@@ -1,24 +1,38 @@
 import React, {useMemo} from 'react'
 import { useNavigate, useParams, Link } from "react-router-dom"
-import ProductsTable from "../components/ProductsTable"
-import { selectOrders } from '../actions/ordersSlice';
+import { completeOrder, rejectOrder, deleteOrderProduct, selectOrders } from '../actions/ordersSlice';
 import { getOrderByNumber } from '../utils/getOrderByNumber';
-import { useAppSelector } from '../hooks';
+import { useAppDispatch, useAppSelector } from '../hooks';
 
 
 const EditOrderScreen = () => {
-  const {orderid} = useParams()
+  const { orderid } = useParams()
   const ordersList = useAppSelector(selectOrders)
-
   const order = useMemo(() => getOrderByNumber(ordersList, orderid), [orderid, ordersList])
   
+  //handle return
   const navigate = useNavigate()
   const handleReturn = () => {
     navigate(-1)
   }
 
-  const deleteProduct = (productid) => {}
-  
+  //dispatcher
+  const dispatch = useAppDispatch() 
+
+  const deleteProduct = (productid) => {
+    dispatch(deleteOrderProduct({
+      Number: Number(orderid), 
+      productid: Number(productid)
+    }))
+  }
+  const complete = () => {
+    dispatch(completeOrder({Number: Number(orderid)}))
+  }
+
+  const reject = () => {
+    dispatch(rejectOrder({Number: Number(orderid)}))
+  }
+
   return(
     <div className="mb-4">
       <div className="row mt-4">
@@ -51,72 +65,87 @@ const EditOrderScreen = () => {
         </table>
       </div>
 
-      <div>
-        <ProductsTable 
-          productsList={order.products}
-          actions={[{name: 'Delete', action: deleteProduct}]}
-        />
-        <div className='d-flex justify-content-end mb-4'>
-          <Link to={`/products`} className='btn btn-primary'>
-            Add Item+
-          </Link>
-        </div>
-      </div>
-
-      <div className="d-flex justify-content-end">
-        <div className="col-sm-3">
-          <table className="table table-borderless table-sm">
+      <div className='row'>
+        <div className='col mb-5'>        
+          <table className="table table-striped">
+            <thead>
+              <tr>
+                <th scope='col'>N°</th>
+                <th scope='col'>Name</th>
+                <th scope='col'>Quantity</th>
+                <th scope='col'>Unit Price</th>
+                <th scope='col'>Cost</th>
+                <th scope='col'>Actions</th>
+              </tr>
+            </thead>
             <tbody>
-              <tr>
-                <td className="fw-bold">Subtotal</td>
-                <td className="text-end">${order.Subtotal}</td>
-              </tr>
-              <tr>
-                <td className="fw-bold">Taxes</td>
-                <td></td>
-              </tr>
-              <tr>
-                <td colSpan={2}>
-                  <table className="table table-borderless table-sm">
-                    <tbody>
-                      <tr>
-                        <td className="fw-bold">Total City Tax</td>
-                        <td className="text-end">${order.Taxes&&order.Taxes.CityTax}</td>
-                      </tr>
-                      <tr>
-                        <td className="fw-bold">Total County Tax</td>
-                        <td className="text-end">${order.Taxes&&order.Taxes.CountyTax}</td>
-                      </tr>
-                      <tr>
-                        <td className="fw-bold">Total State Tax</td>
-                        <td className="text-end">${order.Taxes&&order.Taxes.StateTax}</td>
-                      </tr>
-                      <tr>
-                        <td className="fw-bold">Total Federal Tax</td>
-                        <td className="text-end">${order.Taxes&&order.Taxes.FederalTax}</td>
-                      </tr>
-                    </tbody>
-                  </table>
-                </td>
-              </tr>
-              <tr>
-                <td className="fw-bold">Total Taxes</td>
-                <td className="text-end">${order.TotalTaxes&&order.TotalTaxes}</td>
-              </tr>
-              <tr>
-                <td className="fw-bold">Total</td>
-                <td className="text-end">${order.Total&&order.Total}</td>
-              </tr>
+              {
+                order.products.map(product => {
+                  return (
+                    <tr key={product.id}>
+                      <td>{product.id}</td>
+                      <td>{product.Name}</td>
+                      <td>{product.Quantity}</td>
+                      <td>${product.Unitprice}</td>
+                      <td>${product.Unitprice*product.Quantity}</td>
+                      <td>
+                        <p
+                          onClick={() => deleteProduct(product.id)}
+                          className='btn btn-link'>Delete
+                        </p>
+                      </td>
+                    </tr>
+                  )
+                })
+              }
             </tbody>
           </table>
-          <div className="d-flex justify-content-between">
-            <button onClick={handleReturn} className='btn btn-success'>
-              Complete Order
-            </button>
+          <div className='d-flex justify-content-end mb-4'>
+            <Link to={`/products`} className='btn btn-primary'>
+              Add Item+
+            </Link>
+          </div>
+        </div>
 
-            <button onClick={handleReturn} className='btn btn-danger'>
-              Reject Order
-            </button>
+        <div className="col-lg-4 d-flex justify-content-end">
+          <div className="">
+            <ul className='list-group mb-4'>
+              <li className='list-group-item d-flex justify-content-between align-items-center'>
+                Subtotal <span className="badge text-dark">${order.Subtotal}</span>
+              </li>
+              <li className='list-group-item align-items-center'>
+                Taxes
+                <ul className='list-group' style={{fontSize: '0.9em'}}>
+                  <li className='list-group-item d-flex justify-content-between align-items-center'>
+                    Total City Tax <span className="badge text-dark">${order.Taxes.CityTax}</span>
+                  </li>
+                  <li className='list-group-item d-flex justify-content-between align-items-center'>
+                    Total County Tax <span className="badge text-dark">${order.Taxes.CountyTax}</span>
+                  </li>
+                  <li className='list-group-item d-flex justify-content-between align-items-center'>
+                    Total State Tax <span className="badge text-dark">${order.Taxes.StateTax}</span>
+                  </li>
+                  <li className='list-group-item d-flex justify-content-between align-items-center'>
+                    Total Federal Tax <span className="badge text-dark">${order.Taxes.FederalTax}</span>
+                  </li>
+                </ul>
+              </li>
+              <li className='list-group-item d-flex justify-content-between align-items-center'>
+                Total Taxes <span className="badge text-dark">${order.TotalTaxes}</span>
+              </li>
+              <li className='list-group-item d-flex justify-content-between align-items-center'>
+                Total <span className="badge text-dark">${order.Total}</span>
+              </li>
+            </ul>
+            <div className="d-flex justify-content-around mb-3">
+              <button onClick={() => complete({Number: order.Number})} className='btn btn-success btn-sm me-2'>
+                Complete Order
+              </button>
+
+              <button onClick={reject} className='btn btn-danger btn-sm'>
+                Reject Order
+              </button>
+            </div>
           </div>
         </div>
       </div>
