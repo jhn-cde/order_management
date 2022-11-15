2bf5517b582c77811b40652b0e3567613e64a5ee
jhn-cde <toe.johan@unsaac.edu.pe>
2022-11-14 22:02:02 -0500

    change name to getRange

---
+++ cliente/src/utils/getRange.js
@@ -0,0 +1,9 @@
+export const getRange = (data, rowsPerPage) => {
+  const range = [];
+  const num = Math.ceil(data.length / rowsPerPage);
+  
+  for (let i = 1; i <= num; i++) {
+    range.push(i);
+  }
+  return range;
+};
