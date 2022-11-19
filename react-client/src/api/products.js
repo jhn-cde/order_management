1835ea399f6cfb628d64f9a947c46d1b492a3a96
jhn-cde <toe.johan@unsaac.edu.pe>
2022-11-18 23:36:43 -0500

    fetch products

---
+++ react-client/src/api/products.js
@@ -0,0 +1,46 @@
+import { setProductsSlice } from "../products/actions/productsPageSlice";
+import { setProducts, setProduct } from "../products/actions/productsSlice";
+import { api } from "./api";
+
+export const fetchProducts = () => {
+  return async (dispatch) => {
+    api
+      .get('/api/products')
+      .then((response) => {
+        dispatch(setProducts(response.data));
+      })
+      .catch((err) => {
+        console.log('fetchproducts', err)
+        dispatch(setProducts([]));
+      });
+  };
+}
+
+export const fetchProduct = ({id}) => {
+  return async (dispatch) => {
+    api
+      .get(`/api/product/${id}`)
+      .then((response) => {
+        dispatch(setProduct(response.data));
+      })
+      .catch((err) => {
+        console.log('fetch product', err)
+        dispatch(setProduct(undefined));
+      });
+  };
+}
+
+export const fetchProductsSlice = ({page, rowsPerPage, searchtext}) => {
+  return async (dispatch) => {
+    api
+      .get('/api/products', {
+        params: {skip: page*rowsPerPage, limit: rowsPerPage, search: searchtext}})
+      .then((response) => {
+        dispatch(setProductsSlice(response.data));
+      })
+      .catch((err) => {
+        console.log('fetchproductsslice', err)
+        dispatch(setProductsSlice([]));
+      });
+  };
+}
