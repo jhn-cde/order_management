43536c7e48c30ec7caa02c982381af5a48091222
jhn-cde <toe.johan@unsaac.edu.pe>
2022-11-18 21:54:42 -0500

    product controllers

---
+++ controllers/productController.js
@@ -0,0 +1,67 @@
+const ProductModel = require('../models/productModel')
+const product = require('../models/productModel')
+
+//list of products
+exports.product_list = (req, res, next) => {
+  const searchtext = req.query.search?req.query.search : ''
+  const skip = req.query.skip?req.query.skip : 0
+  const limit = req.query.skip?req.query.limit : 0
+  product.find({"Name": {"$regex": searchtext, "$options": "i"}})
+    .sort({id: 1})
+    .skip(skip)
+    .limit(limit)
+    .exec((err, list_products) => {
+      if(err){
+        return next(err)
+      }
+      res.send(list_products)
+  })
+}
+
+//product detail
+exports.product_detail = (req, res) => {
+  product.findOne({id: req.params.id})
+    .exec((err, product_detail) => {
+      if(err){
+        return next(err)
+      }
+      res.send(product_detail)
+    })
+}
+
+// Handle product create on post
+exports.product_create_post = (req, res, next) => {
+  //data is valid
+  const newProduct = new ProductModel(req.body)
+  newProduct.save((err) => {
+    if(err) {
+      return next(err)
+    }
+    //successful
+    res.send(`Product created successfully with id ${newProduct.id}`)
+  })
+}
+
+// Handle product update on POST.
+exports.product_update_post = (req, res) => {
+  let toUpdate = {...req.body}
+  ProductModel.findOneAndUpdate({id: req.params.id}, toUpdate, (err, order) => {
+    if(!err){
+      res.send(`Product with id ${req.params.id} updated successfully`)
+    }else{
+      console.log('error! update product ', err)
+      res.send(err)
+    }
+  })
+};
+
+// Handle product delete on POST.
+exports.product_delete_post = (req, res) => {
+  product.findOneAndDelete({id: req.params.id})
+    .exec((err) => {
+      if(err){
+        return next(err)
+      }
+      res.send(`Product with id ${req.params.id} deleted`)
+    })
+};
