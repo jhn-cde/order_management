fc447b14c2ad5e4b8eb66a97b208b81f3e3f5b34
jhn-cde <toe.johan@unsaac.edu.pe>
2022-11-18 22:57:03 -0500

    name order-management

--- react-client/src/actions/api.js
+++
@@ -1,8 +0,0 @@
-export default api = axios.create({
-  baseURL: "https://order-management-jhn-cde.vercel.app/",
-  withCredentials: false,
-  headers: {
-    Accept: "application/json",
-    "Content-Type": "application/json",
-  },
-});
