2c121fa7aaab38cb9304eeaa602621711dc95aa9
jhn-cde <toe.johan@unsaac.edu.pe>
2022-11-19 04:55:00 -0500

    update product

--- react-client/src/api/products.js
+++ react-client/src/api/products.js
@@ -1,17 +1,16 @@
-import { setProductsSlice } from "../products/actions/productsPageSlice";
-import { setProducts, setProduct } from "../products/actions/productsSlice";
+import { setProducts, setProduct, setProductsCount } from "../products/actions/productsSlice";
 import { api } from "./api";
 
-export const fetchProducts = () => {
+export const fetchProductsCount = () => {
   return async (dispatch) => {
     api
-      .get('/api/products')
+      .get('/api/products/count')
       .then((response) => {
-        dispatch(setProducts(response.data));
+        dispatch(setProductsCount(response.data.count));
       })
       .catch((err) => {
-        console.log('fetchproducts', err)
-        dispatch(setProducts([]));
+        console.log('get count', err)
+        dispatch(setProductsCount(0));
       });
   };
 }
@@ -30,17 +29,63 @@ export const fetchProduct = ({id}) => {
   };
 }
 
-export const fetchProductsSlice = ({page, rowsPerPage, searchtext}) => {
+export const fetchProducts = ({page, rowsPerPage, searchtext}) => {
   return async (dispatch) => {
     api
       .get('/api/products', {
         params: {skip: page*rowsPerPage, limit: rowsPerPage, search: searchtext}})
       .then((response) => {
-        dispatch(setProductsSlice(response.data));
+        dispatch(setProducts(response.data));
       })
       .catch((err) => {
-        console.log('fetchproductsslice', err)
-        dispatch(setProductsSlice([]));
+        console.log('fetch products', err)
+        dispatch(setProducts(undefined));
+      });
+  };
+}
+
+export const updateProduct = ({id, toUpdate}) => {
+  return async (dispatch) => {
+    api
+      .post(`/api/product/${id}/update`, toUpdate)
+      .then((response) => {
+        alert(response.data.ans)
+        dispatch(setProduct(response.data.order))
+      })
+      .catch((err) => {
+        if(err){
+          console.log(err) 
+        }
+      });
+  };
+}
+
+export const deleteProduct = (id) => {
+  return async (dispatch) => {
+    api
+      .post(`/api/product/${id}/delete`)
+      .then((response) => {
+        alert(response.data)
+      })
+      .catch((err) => {
+        if(err){
+          console.log(err)
+        }
+      });
+  };
+}
+
+export const createProduct = (product) => {
+  return async (dispatch) => {
+    api
+      .post(`/api/product/create`, product)
+      .then((response) => {
+        alert(response.data)
+      })
+      .catch((err) => {
+        if(err){
+          console.log(err) 
+        }
       });
   };
 }
