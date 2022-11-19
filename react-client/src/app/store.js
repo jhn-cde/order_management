964b3f892a4df3d3bcf5e707bb4a0cf61fa6b222
jhn-cde <toe.johan@unsaac.edu.pe>
2022-11-19 01:15:50 -0500

    products store

--- react-client/src/app/store.js
+++ react-client/src/app/store.js
@@ -1,10 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { ordersSliceReducer } from '../orders/actions/ordersPageSlice'
 import { ordersReducer } from '../orders/actions/ordersSlice'
+import { productsSliceReducer } from '../products/actions/productsPageSlice'
+import { productsReducer } from '../products/actions/productsSlice'
 
 export const store = configureStore({
   reducer: {
     ordersSlice: ordersSliceReducer,
-    orders: ordersReducer
+    orders: ordersReducer,
+
+    productsSlice: productsSliceReducer,
+    products: productsReducer
   }
 })
