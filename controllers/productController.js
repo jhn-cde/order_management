3aef210b556253222427cd0b8554c6118d9a4773
jhn-cde <toe.johan@unsaac.edu.pe>
2022-11-19 05:26:01 -0500

    create product

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,8 +1,20 @@
 const ProductModel = require('../models/productModel')
 const product = require('../models/productModel')
 
+//count documents
+exports.product_count = (req, res, next) => {
+  const query = ProductModel.countDocuments()
+  query.exec((err, count)=>{
+    if(err){
+      return next(err)
+    }
+    res.send({count})
+  })
+}
+
 //list of products
 exports.product_list = (req, res, next) => {
+  
   const searchtext = req.query.search?req.query.search : ''
   const skip = req.query.skip?req.query.skip : 0
   const limit = req.query.skip?req.query.limit : 0
@@ -31,23 +43,34 @@ exports.product_detail = (req, res) => {
 
 // Handle product create on post
 exports.product_create_post = (req, res, next) => {
-  //data is valid
-  const newProduct = new ProductModel(req.body)
-  newProduct.save((err) => {
+  const query = ProductModel.countDocuments()
+  query.exec((err, count)=>{
     if(err) {
       return next(err)
     }
-    //successful
-    res.send(`Product created successfully with id ${newProduct.id}`)
+    const newProduct = new ProductModel({
+      ...req.body,
+      id: count+1
+    })
+    newProduct.save((err) => {
+      if(err) {
+        return next(err)
+      }
+      //successful
+      res.send(`Product created successfully with id ${newProduct.id}`)
+    })
   })
 }
 
 // Handle product update on POST.
 exports.product_update_post = (req, res) => {
   let toUpdate = {...req.body}
-  ProductModel.findOneAndUpdate({id: req.params.id}, toUpdate, (err, order) => {
+  ProductModel.findOneAndUpdate({id: req.params.id}, toUpdate, (err, product) => {
     if(!err){
-      res.send(`Product with id ${req.params.id} updated successfully`)
+      res.send({
+        order: {...product._doc, ...toUpdate},
+        ans: `Product with id ${req.params.id} updated successfully`
+      })
     }else{
       console.log('error! update product ', err)
       res.send(err)
@@ -56,7 +79,7 @@ exports.product_update_post = (req, res) => {
 };
 
 // Handle product delete on POST.
-exports.product_delete_post = (req, res) => {
+exports.product_delete_post = (req, res, next) => {
   product.findOneAndDelete({id: req.params.id})
     .exec((err) => {
       if(err){
