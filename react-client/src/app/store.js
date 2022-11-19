85652514f27ff5c0619c0af8a650b31a275680a1
jhn-cde <toe.johan@unsaac.edu.pe>
2022-11-19 04:55:40 -0500

    remove extra slice

--- react-client/src/app/store.js
+++ react-client/src/app/store.js
@@ -1,15 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
-import { ordersSliceReducer } from '../orders/actions/ordersPageSlice'
 import { ordersReducer } from '../orders/actions/ordersSlice'
-import { productsSliceReducer } from '../products/actions/productsPageSlice'
 import { productsReducer } from '../products/actions/productsSlice'
 
 export const store = configureStore({
   reducer: {
-    ordersSlice: ordersSliceReducer,
     orders: ordersReducer,
-
-    productsSlice: productsSliceReducer,
     products: productsReducer
   }
 })
